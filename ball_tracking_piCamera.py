98e2da1018b677919f32ee8bf3c9ad9df51b600f
shaaofbla <daniel.b.schmocker@gmail.com>
2017-10-05 17:38:17 +0000

    transmission of the coordinates over tcp added. a bit slow

--- ball_tracking_piCamera.py
+++ ball_tracking_piCamera.py
@@ -4,6 +4,21 @@ from collections import deque
 import numpy as np
 import time
 import cv2
+import socket
+
+#TCP settings
+TCP_IP = '192.168.0.11'
+TCP_PORT = 5005
+BUFFER_SIZE = 60
+SEND2TCP = True
+
+#Output settings
+SHOW = False
+SHOW_CIRCLE = False
+SHOW_PATH = False
+
+sok = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sok.connect((TCP_IP, TCP_PORT))
 
 #initialize picamera
 camera = PiCamera()
@@ -40,21 +55,25 @@ for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_por
         if cv2.contourArea(c)< 500:
             rawCapture.truncate(0)
             continue
-            p = "continue"
 
-        print p
-        
         if radius > 10:
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255,255), 2)
-            cv2.circle(frame, center, 5, (0,0,255), -1)
-    pts.appendleft(center)
-    for i in xrange(1, len(pts)):
-        if pts[i-1] is None or pts[i] is None:
-            continue
-        thickness = int(np.sqrt(64 / float(i+1)) * 2.5)
-        cv2.line(frame, pts[i-1], pts[i], (0,0,255), thickness)
+            if SHOW_CIRCLE:
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255,255), 2)
+                cv2.circle(frame, center, 5, (0,0,255), -1)
+            if SEND2TCP:
+                message = "{0}\t{1}\t{2}".format(round(x,2),round(y,2),round(radius,2))
+                sok.send(message)
 
-    cv2.imshow("Frame", frame)
+    pts.appendleft(center)
+    if SHOW & SHOW_PATH:
+        for i in xrange(1, len(pts)):
+            if pts[i-1] is None or pts[i] is None:
+                continue
+            thickness = int(np.sqrt(64 / float(i+1)) * 2.5)
+            cv2.line(frame, pts[i-1], pts[i], (0,0,255), thickness)
+
+    if SHOW:
+        cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
     rawCapture.truncate(0)
@@ -62,4 +81,5 @@ for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_por
     if key == ord("q"):
         break
 
+sok.close()
 cv2.destroyAllWindows()
