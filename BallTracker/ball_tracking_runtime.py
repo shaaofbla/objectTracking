440fd98d358daee78c0ffde5d8e99272dcedb911
shaaofbla <daniel.b.schmocker@gmail.com>
2017-10-11 17:06:05 +0000

    some changes int the client file

--- BallTracker/ball_tracking_runtime.py
+++ BallTracker/ball_tracking_runtime.py
@@ -33,7 +33,7 @@ while True:
         print "restarting client"
         TCPclient.close()
         del TCPclient
-        TCPclient = client()
+        TPclient = client()
         TCPclient.config()
         TCPclient.connect()
         continue
